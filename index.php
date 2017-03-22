f5bea19613983b205eba4fab617dc876c928c406
nopphasin <nopphasin0508@gmail.com>
2017-03-22 21:23:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <html>
-<center>
+<head>
+	<center>
   <br> </br>
   <br> </br>
 <h1><b>Welcome to Smart Hydroponics Gardening Kit !</b></h1>
   <br> </br>
    <br> </br>
+</head>
+<body>
 <div class="item">
 					<div class="row">
 						<div class="col-md-offset-2 col-md-8">
-							<img src="https://www.dropbox.com/s/vdjph82o4f3r19x/home-bg.jpg?dl=0" class="img-responsive img-circle" alt="testimonial img">
+							<img src="https://www.img.in.th/images/bc71a71b70ef72dd7282206c42039d01.jpg" border="0" class="img-responsive img-circle" alt="testimonial img">
             </div></div></div>
 
 </center>
+</body>
 </html>
