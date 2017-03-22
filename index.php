aac74c0c8b5acbddc38bfe3690dead952e0f5cdc
nopphasin <nopphasin0508@gmail.com>
2017-03-22 21:27:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,21 @@
    <br> </br>
 </head>
 <body>
+	<section id="testimonial" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<!-- Service Owl Carousel section
+			================================================== -->
+			<div id="owl-testimonial" class="owl-carousel">
+
 <div class="item">
 					<div class="row">
 						<div class="col-md-offset-2 col-md-8">
 							<img src="https://www.img.in.th/image/Cs27" border="0" class="img-responsive img-circle" alt="testimonial img">
             </div></div></div>
+				</div>
+</section>
 
 </center>
 </body>
