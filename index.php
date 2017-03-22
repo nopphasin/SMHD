48764c4ed03d92f3e8dda9f616297e3707aabd3a
nopphasin <nopphasin0508@gmail.com>
2017-03-22 21:43:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<script src="http://yecccmu-test.herokuapp.com/owl.carousel.css"></script>
+
 	<center>
   <br> </br>
   <br> </br>
@@ -9,24 +9,5 @@
    <br> </br>
 </center>
 </head>
-<body>
-	<center>
-	<section id="testimonial" class="parallax-section">
-	<div class="container">
-		<div class="row">
 
-			<!-- Service Owl Carousel section
-			================================================== -->
-			<div id="owl-testimonial" class="owl-carousel">
-
-<div class="item">
-					<div class="row">
-						<div class="col-md-offset-2 col-md-8">
-							<img src="https://www.img.in.th/images/9a420f6a04ec7250c5d65aceced6ddac.jpg" border="0" class="img-responsive img-circle" alt="testimonial img">
-            </div></div></div>
-				</div>
-</section>
-
-</center>
-</body>
 </html>
