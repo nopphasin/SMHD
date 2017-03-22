130b1e842cba40ff0a7266b0b6fd58ead2061b15
nopphasin <nopphasin0508@gmail.com>
2017-03-22 21:26:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <div class="item">
 					<div class="row">
 						<div class="col-md-offset-2 col-md-8">
-							<img src="https://www.img.in.th/images/bc71a71b70ef72dd7282206c42039d01.jpg" border="0" class="img-responsive img-circle" alt="testimonial img">
+							<img src="https://www.img.in.th/image/Cs27" border="0" class="img-responsive img-circle" alt="testimonial img">
             </div></div></div>
 
 </center>
